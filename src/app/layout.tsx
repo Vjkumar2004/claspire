161f5b543892ef9acd22b7f29b3c6dbc86d65717
Vjkumar2004 <vijay909232@gmail.com>
2026-03-24 20:42:00 +0530

    Add Google Analytics tracking (G-JFRRHTC04F)
    - Install Google Analytics tag in root layout
    - Add gtag.js script with tracking ID G-JFRRHTC04F
    - Tag will be present on all pages for analytics data collection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -85,6 +85,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google tag (gtag.js) */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JFRRHTC04F"></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-JFRRHTC04F');
+            `
+          }}
+        />
         <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="icon" href="/favicon-32x32.png" sizes="32x32" type="image/png" />
         <link rel="icon" href="/favicon-16x16.png" sizes="16x16" type="image/png" />
